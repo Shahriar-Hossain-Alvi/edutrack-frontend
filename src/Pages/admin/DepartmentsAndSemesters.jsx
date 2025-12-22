118581cadccebb183f3e2b07c2f55a42beee9d4e
Shahriar-Hossain-Alvi <shahriarhossainalvi@gmail.com>
2025-12-22 19:47:32 +0600

    added department create, delete, update functions with modals

--- src/Pages/admin/DepartmentsAndSemesters.jsx
+++ src/Pages/admin/DepartmentsAndSemesters.jsx
@@ -3,14 +3,24 @@ import useAxiosSecure from '../../hooks/useAxiosSecure.jsx';
 import { DepartmentsSkeleton, SemestersSkeleton } from '../../components/ui/Skeletons.jsx';
 import { FaEdit } from 'react-icons/fa';
 import { MdDelete } from 'react-icons/md';
-import { FaPlus } from 'react-icons/fa6';
 import { useForm } from 'react-hook-form';
 import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 import toast from 'react-hot-toast';
+import { useState } from 'react';
+import CreateDepartment from '../../components/pageComponents/DepartmentPage/CreateDepartment.jsx';
 
 const DepartmentsAndSemesters = () => {
     const axiosSecure = useAxiosSecure();
-    const { register, handleSubmit, formState: { errors, isLoading: formLoading } } = useForm();
+    const { register, handleSubmit, formState: { errors, isLoading: formLoading }, reset } = useForm();
+    const [selectedDept, setSelectedDept] = useState(null); // state for editing
+
+    const openUpdateModal = (dept) => {
+        setSelectedDept(dept);
+        reset({ updatedDepartmentName: dept.department_name }); // This fills the form
+        // @ts-ignore
+        document.getElementById('update_dept_modal').showModal();
+    }
+
 
     // DEPARTMENTS query
     const { data: allDepartments, isLoading, error, isError, refetch: allDepartmentsRefetch } = useQuery({
@@ -30,20 +40,50 @@ const DepartmentsAndSemesters = () => {
         }
     })
 
-    const addNewDepartment = async (data) => {
+
+    // const Delete Department
+    const deleteDepartment = async (id) => {
         try {
-            const res = await axiosSecure.post('/departments', { department_name: data.departmentName });
+            // @ts-ignore
+            document.getElementById('delete_dept_modal').close();
+            const res = await axiosSecure.delete(`/departments/${id}`);
             console.log(res);
             allDepartmentsRefetch();
-            // @ts-ignore
-            document.getElementById('my_modal_1').close();
             // @ts-ignore
             toast.success(res?.data?.message);
         } catch (error) {
             console.log(error);
-            toast.error('Failed to add department');
+            // @ts-ignore
+            document.getElementById('delete_dept_modal').close();
+            toast.error(error?.response?.data?.detail || 'Failed to delete department');
         }
     }
+
+
+    // update department name
+    const updateDepartment = async (data) => {
+        const updated_name = data.updatedDepartmentName;
+        const updated_dept_id = selectedDept.id;
+
+        console.log("ID: ", updated_dept_id, "Changed Name: ", updated_name);
+        try {
+            // @ts-ignore
+            document.getElementById('update_dept_modal').close();
+            const res = await axiosSecure.patch(`/departments/${updated_dept_id}`, { department_name: updated_name });
+            console.log(res);
+            allDepartmentsRefetch();
+            // @ts-ignore
+            toast.success(res?.data?.message);
+        } catch (error) {
+            console.log(error);
+            // @ts-ignore
+            document.getElementById('update_dept_modal').close();
+            toast.error(error?.response?.data?.detail || 'Failed to delete department');
+        } finally {
+            reset();
+        }
+    }
+
 
     return (
         <div>
@@ -52,45 +92,14 @@ const DepartmentsAndSemesters = () => {
                 <div className='flex justify-between'>
                     <h1 className='text-3xl font-bold'>Departments</h1>
 
-                    {/* Open the modal using document.getElementById('ID').showModal() method */}
-                    <button className='btn btn-ghost btn-sm group/add-dept hover:bg-transparent border-0 tooltip tooltip-left' data-tip="Add New Department" onClick={() => document.getElementById('my_modal_1').
-                        // @ts-ignore
-                        showModal()}>
-                        <FaPlus className='text-lg group-hover/add-dept:text-success' />
-                    </button>
-                    <dialog id="my_modal_1" className="modal">
-                        <div className="modal-box">
-                            <h3 className="font-bold text-lg">Add New Department</h3>
-
-                            <div className="modal-action flex-col">
-                                {/* FORM to add new department */}
-                                <form className='space-y-4' onSubmit={handleSubmit(addNewDepartment)}>
-                                    <div className={`${errors.departmentName && "tooltip tooltip-open tooltip-bottom tooltip-error"} w-full`} data-tip={errors.departmentName && errors.departmentName.message}>
-                                        <label>Department Name</label>
-                                        <input
-                                            type="text"
-                                            placeholder="Department Name"
-                                            className="input input-bordered w-full mt-2"
-                                            {...register("departmentName", { required: "Department name is required" })}
-                                        />
-                                    </div>
-                                    <button className={`${formLoading && "btn-disabled"} btn w-full btn-success`}>
-                                        {formLoading ? <AiOutlineLoading3Quarters className='animate-spin' /> : "Create"}
-                                    </button>
-                                </form>
-
-                                <form method="dialog" className='flex justify-between items-center'>
-                                    <p className='text-base'>Press ESC key or click outside to close</p>
-                                    {/* if there is a button in form, it will close the modal */}
-                                    <button className="btn btn-error mt-3">Close</button>
-                                </form>
-                            </div>
-                        </div>
-                    </dialog>
+                    <CreateDepartment allDepartmentsRefetch={allDepartmentsRefetch} />
                 </div>
 
                 {isLoading && <DepartmentsSkeleton />}
                 {isError && <h4 className='text-error text-center text-lg'>An Error Occurred: {error?.message}</h4>}
+
+
+                {/* departments table */}
                 {
                     allDepartments?.length === 0 ?
                         <h4 className='text-error text-center text-lg'>No departments found</h4>
@@ -114,13 +123,29 @@ const DepartmentsAndSemesters = () => {
                                                 <th>{index + 1}</th>
                                                 <td>{department?.department_name.toUpperCase()}</td>
                                                 <td>{department?.id}</td>
-                                                <td>
-                                                    <button className="btn btn-ghost hover:bg-transparent border-0 group/edit-dept">
-                                                        <FaEdit className='group-hover/edit-dept:text-success' />
+                                                <td className='flex gap-2'>
+                                                    {/* update department Modal trigger */}
+                                                    <button
+                                                        onClick={() => openUpdateModal(department)}
+                                                        className="btn btn-ghost hover:bg-transparent border-0 group/edit-dept">
+                                                        <FaEdit className='group-hover/edit-dept:text-success'
+                                                        />
                                                     </button>
-                                                    <button className="btn btn-ghost hover:bg-transparent border-0  group/delete-dept">
-                                                        <MdDelete className='group-hover/delete-dept:text-red-700 text-lg' />
-                                                    </button>
+
+
+                                                    {/* Delete Department confirmation Modal */}
+                                                    <div>
+                                                        <button className="btn btn-ghost hover:bg-transparent border-0 group/delete-dept"
+                                                            onClick={() => {
+                                                                setSelectedDept(department);
+                                                                document.getElementById('delete_dept_modal').
+                                                                    // @ts-ignore
+                                                                    showModal()
+                                                            }}
+                                                        >
+                                                            <MdDelete className='group-hover/delete-dept:text-red-700 text-lg' />
+                                                        </button>
+                                                    </div>
                                                 </td>
                                             </tr>
                                         )
@@ -131,6 +156,47 @@ const DepartmentsAndSemesters = () => {
                         </div>
                 }
             </div>
+
+            {/* Update department modal */}
+            <dialog id="update_dept_modal" className="modal">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg text-warning">Edit Department</h3>
+                    <form className='space-y-3 mt-4' onSubmit={handleSubmit(updateDepartment)}>
+                        <div className={`${errors.updatedDepartmentName && "tooltip tooltip-open tooltip-bottom tooltip-error"} w-full`} data-tip={errors.updatedDepartmentName && errors.updatedDepartmentName.message}>
+                            <label>New Department Name</label>
+                            <input
+                                type="text"
+                                className="input input-bordered w-full mt-2"
+                                {...register("updatedDepartmentName", { required: "Department name is required" })}
+                            />
+                        </div>
+                        <button className="btn btn-success w-full" type='submit' disabled={isLoading}>
+                            {isLoading ? <AiOutlineLoading3Quarters className='animate-spin' /> : "Update"}
+                        </button>
+                    </form>
+                    <div className="modal-action">
+                        <form method="dialog"><button className="btn btn-error">Cancel</button></form>
+                    </div>
+                </div>
+            </dialog>
+
+            <dialog id="delete_dept_modal" className="modal">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg text-error uppercase">Confirm!!!</h3>
+                    <p className="py-4">Are you sure you want to delete "{selectedDept?.department_name.toUpperCase()}" department?</p>
+
+                    <div className="modal-action">
+                        <button onClick={() => deleteDepartment(selectedDept?.id)} className="btn btn-error">
+                            Yes, delete it
+                        </button>
+                        <form method="dialog"
+                        >
+                            {/* if there is a button in form, it will close the modal */}
+                            <button className="btn">Close</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
 
 
             {/* SEMESTERS */}
@@ -181,7 +247,7 @@ const DepartmentsAndSemesters = () => {
                         </div>
                 }
             </div>
-        </div>
+        </div >
     );
 };
 
---
+++ src/components/pageComponents/DepartmentPage/CreateDepartment.jsx
@@ -0,0 +1,79 @@
+import { FaPlus } from 'react-icons/fa6';
+import useAxiosSecure from '../../../hooks/useAxiosSecure.jsx';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { AiOutlineLoading3Quarters } from 'react-icons/ai';
+
+const CreateDepartment = ({ allDepartmentsRefetch }) => {
+    const axiosSecure = useAxiosSecure();
+    const { register, handleSubmit, formState: { errors, isLoading }, reset } = useForm();
+
+
+    // ADD NEW DEPARTMENT Function
+    const addNewDepartment = async (data) => {
+        console.log(data);
+        try {
+            const res = await axiosSecure.post('/departments', { department_name: data.departmentName });
+            console.log(res);
+            allDepartmentsRefetch();
+            // @ts-ignore
+            document.getElementById('create_dept_modal').close();
+            // @ts-ignore
+            toast.success(res?.data?.message);
+        } catch (error) {
+            console.log(error);
+            // @ts-ignore
+            document.getElementById('create_dept_modal').close();
+            toast.error(error?.response?.data?.detail);
+        } finally {
+            reset();
+        }
+    }
+
+    return (
+        <div>
+            <div>
+                {/* create department modal */}
+                <button className='btn btn-ghost btn-sm group/add-dept hover:bg-transparent border-0 tooltip tooltip-left' data-tip="Add New Department" onClick={() => {
+                    reset(); document.getElementById('create_dept_modal').
+                        // @ts-ignore
+                        showModal()
+                }}>
+                    <FaPlus className='text-lg group-hover/add-dept:text-success' />
+                </button>
+
+                <dialog id="create_dept_modal" className="modal">
+                    <div className="modal-box">
+                        <h3 className="font-bold text-lg">Add New Department</h3>
+
+                        <div className="modal-action flex-col">
+                            {/* FORM to add new department */}
+                            <form className='space-y-4' onSubmit={handleSubmit(addNewDepartment)}>
+                                <div className={`${errors.departmentName && "tooltip tooltip-open tooltip-bottom tooltip-error"} w-full`} data-tip={errors.departmentName && errors.departmentName.message}>
+                                    <label>Department Name</label>
+                                    <input
+                                        type="text"
+                                        placeholder="Department Name"
+                                        className="input input-bordered w-full mt-2"
+                                        {...register("departmentName", { required: "Department name is required" })}
+                                    />
+                                </div>
+                                <button className={`${isLoading && "btn-disabled"} btn w-full btn-success`}>
+                                    {isLoading ? <AiOutlineLoading3Quarters className='animate-spin' /> : "Create"}
+                                </button>
+                            </form>
+
+                            <form method="dialog" className='flex justify-between items-center'>
+                                <p className='text-base'>Press ESC key or click outside to close</p>
+
+                                <button className="btn btn-error mt-3">Close</button>
+                            </form>
+                        </div>
+                    </div>
+                </dialog>
+            </div>
+        </div>
+    );
+};
+
+export default CreateDepartment;
